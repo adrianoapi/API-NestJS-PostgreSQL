4545cd8eeee8182be65a543eff1da570755c4358
Adriano <adrianoapi@hotmail.com>
2020-09-01 17:35:24 -0300

    add auth check credentials

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from '../users/user.repository';
+import { UserRepository } from '../users/users.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserRepository])],
