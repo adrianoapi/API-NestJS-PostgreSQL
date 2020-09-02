1c4f025dc2aa9df024cde78f60ba52a124610377
Adriano <adrianoapi@hotmail.com>
2020-09-02 11:24:30 -0300

    define RolesGuard to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,18 @@ import { Controller, Post, Body, ValidationPipe, UseGuards } from '@nestjs/commo
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ReturnUserDto } from './dto/return-user.dto';
-import { AuthGuard } from '@nestjs/passport'
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from './user-roles.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  @UseGuards(AuthGuard())
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<ReturnUserDto> {
